e744e6f3438f5df2857e36070658c09a148b1247
ArchaicNightfall <ArchaicNightfall@gmail.com>
2025-04-30 21:45:18 -0700

    Dark mode and light mode fix

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ function menuOpen() {
     document.getElementById("mobile_menu").classList.toggle("reveal_mtm")
 }
 
-//darkmode
-function darkMode() {
-    // Select the <link> element
-    let theme = document.getElementById("theme");
-
-    // Toggle between light.css and dark.css
-    if (theme.getAttribute('href') == 'styles.css') {
-        theme.setAttribute('href', 'css/dark_mode.css');
-    } else {
-        theme.setAttribute('href', 'styles.css');
+    //darkmode
+    function darkMode() {
+        // Select the <link> element
+        let theme = document.getElementById("theme");
+    
+        // Toggle between light.css and dark.css
+        if (theme.getAttribute('href') == 'css/styles.css') {
+            theme.setAttribute('href', 'css/dark_mode.css');
+        } else {
+            theme.setAttribute('href', 'css/styles.css');
+        }
     }
-}
